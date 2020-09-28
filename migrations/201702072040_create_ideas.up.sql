202d128cfc9af83b77da78cb7e58390b10ea5046
ybarnovych <yuriy@openvpn.net>
2020-09-28 18:42:19 +0300

    Change limit for post title

--- migrations/201702072040_create_ideas.up.sql
+++ migrations/201702072040_create_ideas.up.sql
@@ -1,6 +1,6 @@
 create table if not exists ideas (
      id           serial primary key,
-     title        varchar(100) not null,
+     title        varchar(1000) not null,
      description  text null,
      tenant_id    int not null,
      created_on   timestamptz not null default now(),
